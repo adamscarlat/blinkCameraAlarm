7095c67050aa53d44b2111346fb31672b0e83801
adamscarlat <adamscarlat@gmail.com>
2021-10-24 11:27:08 -0400

    path?

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 # create, enable and start service
-sudo mv alarm.service /lib/systemd/system/alarm.service
+sudo mv ./alarm.service /lib/systemd/system/alarm.service
 sudo systemctl daemon-reload
 sudo systemctl enable alarm.service
 sudo systemctl start alarm.service
